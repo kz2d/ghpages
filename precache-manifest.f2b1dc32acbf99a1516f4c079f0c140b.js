19576e5e321f19bfaba742a81d48f814a21d814b
kz2d <avidex111@gmail.com>
2020-11-21 16:24:34 +0300

    Updates

---
+++ precache-manifest.f2b1dc32acbf99a1516f4c079f0c140b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe84ecb06927102fb33a25080136b90",
+    "url": "/ghpages/index.html"
+  },
+  {
+    "revision": "382ac1881dafb8ebcc66",
+    "url": "/ghpages/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "13aea13225dbeb494f01",
+    "url": "/ghpages/static/js/2.99511517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages/static/js/2.99511517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382ac1881dafb8ebcc66",
+    "url": "/ghpages/static/js/main.06f58d45.chunk.js"
+  },
+  {
+    "revision": "23c48244537353696d3e",
+    "url": "/ghpages/static/js/runtime-main.dc126614.js"
+  }
+]);
